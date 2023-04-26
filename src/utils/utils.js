b26b01fd646c9b5db905548f92991e880ed16294
nani-samireddy <nanisamireddy05@gmail.com>
2023-04-26 18:02:19 +0530

    updateddd nav

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    var navbar = document.getElementById('NavBar');
-    if (window.pageYOffset > 0) {
-        navbar.classList.add('navblur');
-    } else {
-        navbar.classList.remove('navblur');
-    }
-}
