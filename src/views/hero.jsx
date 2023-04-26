dbfecc553f683e74add101f1c345aef22a951887
nani-samireddy <nanisamireddy05@gmail.com>
2023-04-26 17:00:09 +0530

    completed hero section

--- src/views/hero.jsx
+++ src/views/hero.jsx
@@ -1,12 +1,16 @@
 const Hero = () => {
   return (
     <div className="w-100vh flex justify-center">
-      <div className="w-80p 2xl:w-60vw h-90vh flex flex-col justify-center items-center">
+      <div className="w-80p 2xl:w-60vw min-h-90vh flex flex-col justify-center items-center">
         <h1 className="text-5xl md:text-6xl lg:text-7xl  leading-normal lg:leading-relaxed py-20">
           I’m <span className="font-bold">Nani samireddy</span> - an
           enthusiastic <span className="font-medium">designer & developer</span>{" "}
           based in India.
         </h1>
+        <p className="text-2xl md:text-3xl lg:text-4xl leading-normal lg:leading-relaxed pb-20 ">
+          I’m designer and developer from India with high passion of creating
+          softwares That looks and works great...
+        </p>
       </div>
     </div>
   );
