bd6c270604b6af446bcef83be21cce299a6e8aa2
nani-samireddy <nanisamireddy05@gmail.com>
2023-04-26 18:14:08 +0530

    updated nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import Hero from "./views/hero";
+import Work from "./views/work";
 
 function App() {
   return (
     <div className="text-black ">
       <Navbar />
       <Hero />
+      <Work />
     </div>
   );
 }
--- src/views/work.jsx
+++ src/views/work.jsx
@@ -1,5 +1,11 @@
 const Work = () => {
-  return <>work</>;
+  return (
+    <div id="work" className="w-100vh flex justify-center">
+      <div className="w-80vw 2xl:w-60vw min-h-90vh flex flex-col justify-center items-center">
+        <h1 className="heading">Recent Works</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
