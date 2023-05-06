d53a4ff2e13202b144c1a66e4c8138150946035c
nani samireddy <60937910+nani-samireddy@users.noreply.github.com>
2023-05-06 19:52:29 +0530

    Update make-center.jsx

--- src/components/make-center.jsx
+++ src/components/make-center.jsx
@@ -1,6 +1,6 @@
 const MakeCenter = (props) => {
   return (
-    <div className="w-100vh flex justify-center py-10">
+    <div className="w-100vw flex justify-center py-10">
       <div className="w-80p 2xl:w-60vw min-h-90vh flex flex-col justify-center items-center">
         {props.children}
       </div>
