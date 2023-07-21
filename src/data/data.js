472fc0396d047eece1b9885ce8e08f8a3246af79
nani samireddy <60937910+nani-samireddy@users.noreply.github.com>
2023-07-21 05:10:36 +0000

    updated url

--- src/data/data.js
+++ src/data/data.js
@@ -52,13 +52,22 @@ export const data = {
   initiatives: [
     {
       id: 1,
+      title: 'Open Portfolio | Opensource',
+      description: 'Started a coding community to help students and developers to learn and grow together. We have 500+ members in our community. We conduct weekly coding challenges and monthly hackathons. We also conduct workshops and webinars to help students and developers to learn new technologies. We also have an Instagram page (@thecodeslate) where we post daily coding challenges and tips.',
+      link: 'https://github.com/nani-samireddy/open-portfolio',
+      techUsed: ['ReactJs', 'TailwindCSS', 'Git', 'Github', 'Figma'],
+
+
+  },
+    {
+      id: 2,
       title: "Code Slate | Coding Community",
       description:
         "Started a coding community to help students and developers to learn and grow together. We have 500+ members in our community. We conduct weekly coding challenges and monthly hackathons. We also conduct workshops and webinars to help students and developers to learn new technologies. We also have an Instagram page (@thecodeslate) where we post daily coding challenges and tips.",
       link: "https://instagram.com/thecodeslate",
     },
     {
-      id: 1,
+      id: 3,
       title: "MD Editor | Online Markdown Editor",
       description:
         "An Online MarkDown editor that enables users to Edit and preview the Markdown code.",
