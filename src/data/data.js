9e73dd5a9f0f55f373c213f740741163e9ed4794
nani samireddy <60937910+nani-samireddy@users.noreply.github.com>
2023-07-21 05:13:48 +0000

    updated desc

--- src/data/data.js
+++ src/data/data.js
@@ -53,7 +53,7 @@ export const data = {
     {
       id: 1,
       title: 'Open Portfolio | Opensource',
-      description: 'Started a coding community to help students and developers to learn and grow together. We have 500+ members in our community. We conduct weekly coding challenges and monthly hackathons. We also conduct workshops and webinars to help students and developers to learn new technologies. We also have an Instagram page (@thecodeslate) where we post daily coding challenges and tips.',
+      description: 'Open source portfolio template for developers based on ReactJs and TailwindCSS. Currently version 1.0 ',
       link: 'https://github.com/nani-samireddy/open-portfolio',
       techUsed: ['ReactJs', 'TailwindCSS', 'Git', 'Github', 'Figma'],
 
