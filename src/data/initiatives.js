d093f8fb7523e0605f933ff776d6ece0d467c1d6
nani samireddy <60937910+nani-samireddy@users.noreply.github.com>
2023-07-21 05:08:41 +0000

    updated url

--- src/data/initiatives.js
+++ src/data/initiatives.js
@@ -3,7 +3,7 @@ export const initiativesData = [
         id: 1,
         title: 'Open Portfolio | Opensource',
         description: 'Started a coding community to help students and developers to learn and grow together. We have 500+ members in our community. We conduct weekly coding challenges and monthly hackathons. We also conduct workshops and webinars to help students and developers to learn new technologies. We also have an Instagram page (@thecodeslate) where we post daily coding challenges and tips.',
-        link: 'https://instagram.com/thecodeslate',
+        link: 'https://github.com/nani-samireddy/open-portfolio',
         techUsed: ['ReactJs', 'TailwindCSS', 'Git', 'Github', 'Figma'],
 
 
